d0cba48741697254f2a0f35b864ee7ec66ece035
cam <cambo9p.11@gmail.com>
2024-12-10 19:02:34 +1300

    finished d3p2

--- src/d3.c
+++ src/d3.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../include/lib.h"
+//#include "../include/lib.h"
 
 #define NUM_LINES 1000
 
 int start_parse(char* buffer);
 
+int parse_2(char *buffer, int *swap);
 
 void solve_p1(char* buffer, size_t len) {
     int sum = 0;
@@ -21,9 +22,10 @@ void solve_p1(char* buffer, size_t len) {
 
 void solve_p2(char* buffer, size_t len) {
     int sum = 0;
+    int swap = 0;
 
     while (*buffer != '\0') {
-        sum += start_parse(buffer);
+        sum += parse_2(buffer, &swap);
         buffer++;
     }
     printf("sum = %d\n", sum);
@@ -31,6 +33,7 @@ void solve_p2(char* buffer, size_t len) {
 }
 
 int start_parse(char* buffer) {
+    
     long val1;
     long val2;
     int mul_len = 4;
@@ -54,11 +57,57 @@ int start_parse(char* buffer) {
     return val1 * val2;
 }
 
-int parse_2(char *buffer) {
+// part two 
+
+// 1 if we are skipping 
+// 0 if nothing
+// -1 if we hit a do
+// i gave up writing this nicely
+int parse_d(char*buffer, int* swap) {
+    int mul_len = 2;
+    char check[] = "do";
+    int sec_len = 5;
+    char sec[] = "n't()";
+
+    for (int i = 0; i < mul_len; i++) {
+        if (buffer[0] != check[i]) {
+            return 0;
+        }
+        buffer++;
+    }
+    
+    switch (*buffer) {
+        case '(':
+            return ++*buffer == ')' ? -1 : 0;
+        case 'n':
+            for (int i = 0; i < sec_len; i++) {
+                if (buffer[0] != sec[i]) {
+                    return 0;
+                }
+                buffer++;
+            }
+            return 1;
+        default:
+            return 0;
+            break;
+    }
+}
+
+int parse_2(char *buffer, int *swap) {
+    int tmp = 0;
+
     switch (*buffer) {
         case 'm':
+            if (*swap == 0 || *swap == -1) {
+               int test = start_parse(buffer);
+               return test;
+            }
             break;
         case 'd':
+            tmp = parse_d(buffer, swap);
+            if (tmp != 0) {
+                *swap = tmp;
+            }
             break;
     }
     return 0;
