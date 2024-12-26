1503024ee36d14eb327605636e7ca90ad958df2d
Cameron Pearce <cpe79@uclive.ac.nz>
2024-12-26 20:46:48 +1300

    finish d7

--- src/d7.c
+++ src/d7.c
@@ -35,35 +35,53 @@ void remove_head(long long *arr1, long long *arr2, int len) {
     }
 }
 
+long long concatenate_long_longs(long long a, long long b) {
+    char str_a[32], str_b[32];
+    
+    sprintf(str_a, "%lld", a);
+    sprintf(str_b, "%lld", b);
+
+    char result[64]; 
+    strcpy(result, str_a);
+    strcat(result, str_b);
+
+    long long concatenated_value = strtoll(result, NULL, 10);
+
+    return concatenated_value;
+}
+
 // recustion boi
 // we are doing this backwards so i dont need to reallocate some memory
 // e.g 1 4 6 will get split into two calls 6 + ( 1 4 ) and 6 * ( 1 4 )
 // and if one gets the answer we will return true
-int evaluate_line( long long *arr, int size,  long long goal) {
+int evaluate_line(long long *arr, int size, long long goal) {
     if (size == 1) {
-        return arr[0] == goal; 
+        return arr[0] == goal;
     }
 
-    int tmp = arr[0]; // head of the array
-                      
-     long long *add_arr = ( long long *)calloc(size - 1,sizeof( long long));
-     long long *mul_arr = ( long long *)calloc(size - 1,sizeof( long long));
+    long long tmp = arr[0]; // head of the array
+
+    long long *add_arr = (long long *)calloc(size - 1, sizeof(long long));
+    long long *mul_arr = (long long *)calloc(size - 1, sizeof(long long));
+    long long *conc_arr = (long long *)calloc(size - 1, sizeof(long long));
 
     remove_head(arr, add_arr, size);
     remove_head(arr, mul_arr, size);
+    remove_head(arr, conc_arr, size);
 
     add_arr[0] = tmp + add_arr[0];
     mul_arr[0] = tmp * mul_arr[0];
-    //printf("test\n");
-    //print_arr(size - 1, mul_arr);
-    //print_arr(size - 1, add_arr);
+    conc_arr[0] = concatenate_long_longs(tmp, conc_arr[0]);
 
     int add = evaluate_line(add_arr, size - 1, goal);
     int mul = evaluate_line(mul_arr, size - 1, goal);
+    int conc = evaluate_line(conc_arr, size - 1, goal);
+
     free(add_arr);
     free(mul_arr);
+    free(conc_arr);
 
-    return mul || add;
+    return mul || add || conc;
 }
 
 long long process_line(char* line) {
